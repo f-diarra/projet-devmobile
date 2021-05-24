f01dfd124ea2d50e42fb9a97842efe3d481460da
f-diarra <74508829+f-diarra@users.noreply.github.com>
2021-05-24 18:34:52 +0000

    Update index.js

--- imcCalculator/www/js/index.js
+++ imcCalculator/www/js/index.js
@@ -1,27 +1,27 @@
 function calculIMC(){
     
     let imc = poids.value / Math.pow(taille.value, 2); 
-    let message = "votre imc est de :"+ imc.toFixed(2) +"</br>";
-    message += "vous êtes en de: "
+    let message = "votre imc est de : "+ imc.toFixed(2) +"</br>";
+    message += "vous êtes "
 
     if (poids.value.trim() != "" && taille.value.trim() != "") {
         if (imc < 16.5) {
-            message += "Dénutrition ou anoroxie";
+            message += "en Dénutrition ou anoroxie";
         }else if (imc >= 16.5 && imc < 18.5 ) {
             message += "maigre";
         }else if (imc >= 18.5 && imc < 25 ) {
-            message += "Pois normal";
+            message += "en Poids normal";
         }else if (imc >= 25 && imc < 30 ) {
-            message += "Surpoids";
+            message += "en Surpoids";
         }else if (imc >= 18.5 && imc < 25 ) {
-            message += "Obésité modérée";
+            message += "en Obésité modérée";
         }else if (imc >25 ) {
-            message += "Obésité exéssive";
+            message += "en Obésité exéssive";
         }
     
         diagnostique.innerHTML = message;
     } else {
-        diagnostique.innerHTML = "Veilleur renseigner les données";
+        diagnostique.innerHTML = "Veuilleur renseigner les données";
     }
 
 }
